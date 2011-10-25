62e2748c12fd924e204fd6c63c2ed21ad329396c
Alberto Covarrubias <acovarrubias@ign.com>
2011-10-24 19:19:28 -0700

    It runs!!!

--- streams-skynet/src/main/scala/Main.scala
+++ streams-skynet/src/main/scala/Main.scala
@@ -1,12 +1,17 @@
 package com.ign.hackweek.skynet.streams
 
-
 import twitter4j.{TwitterStream,TwitterStreamFactory}
+import twitter4j.auth.{AccessToken}
+import twitter4j.conf.{Configuration,ConfigurationBuilder}
 
 object Main {
 	def main(args: Array[String]) {
-		TwitterStreamFactory.getSingleton.addListener(TwitterListener)
-		//stream.sample()
+		val builder: ConfigurationBuilder = new ConfigurationBuilder
+		val config: Configuration = builder.setUser("skynetign").setPassword("abcd1234").build
+		val streamFactory: TwitterStreamFactory = new TwitterStreamFactory(config)
+		val stream: TwitterStream = streamFactory.getInstance
+		stream.addListener(TwitterListener)
+		stream.sample
 		//Config.streams.foreach(println)
 	}
 }
--- streams-skynet/src/main/scala/TwitterListener.scala
+++ streams-skynet/src/main/scala/TwitterListener.scala
@@ -1,7 +1,11 @@
 package com.ign.hackweek.skynet.streams
 
-import twitter4j.{Status,StatusListener}
+import twitter4j.{Status,StatusListener,StatusDeletionNotice}
 
 object TwitterListener extends StatusListener {
 	override def onStatus(status: Status) = println(status.getUser.getName + " : " + status.getText)
+	override def onScrubGeo(userId: Long, upToStatusId: Long) = println("onScrub")
+	override def onTrackLimitationNotice(number: Int) = println("onTrack")
+	override def onDeletionNotice(notice: StatusDeletionNotice) = println("onDeletion")
+	override def onException(ex: Exception) = println("onException")
 }
